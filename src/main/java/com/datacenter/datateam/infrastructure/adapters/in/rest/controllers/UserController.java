19eaf8ba835057eae51cb47099fd2cc51126945a
Jair De Jesus Uribe Valencia <143142760+Dreyveringx@users.noreply.github.com>
2025-02-18 09:45:53 -0500

    Update UserControlle.java

--- src/main/java/com/datacenter/datateam/infrastructure/adapters/in/rest/controllers/UserController.java
+++ src/main/java/com/datacenter/datateam/infrastructure/adapters/in/rest/controllers/UserController.java
@@ -2,21 +2,12 @@ package com.datacenter.datateam.infrastructure.adapters.in.rest.controllers;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.datacenter.datateam.application.useCases.RegisterUserUseCase;
-import com.datacenter.datateam.infrastructure.adapters.in.rest.controllers.requests.RegisterUserRequest;
-import com.datacenter.datateam.infrastructure.adapters.in.rest.controllers.responses.UserResponse;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final RegisterUserUseCase registerUserUseCase;
-
-    @PostMapping("/register")
-    public ResponseEntity<UserResponse> registerUser(@RequestBody RegisterUserRequest request) {
-        return ResponseEntity.ok(registerUserUseCase.execute(request));
-    }
+     
 }
